7d3e623c5665ef95536271612f32db87a45c7e65
Mukesh <Stunty@007>
2023-08-06 20:29:02 +0530

    July 23 learning devops

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,68 +1,16 @@
-//def call() {
-//    node {
-//
-//        sh 'env'
-//        common.codequality()
-//
-//
-////        if (branch == "main" || tag ==~ "*" ) {
-////            stage('style checks') {
-////                echo 'code Quality'
-////            }
-////        }
-//  }
-//}
+def call() {
+    node {
 
-
-pipeline {
-
-    agent {
-        node { label 'Workstation' }
-    }
-
-    stages {
-
+        sh 'env'
         common.codequality()
 
-        stage('Style checks') {
 
-            when { tag "*" }
-
-            steps {
-                echo 'style checks'
-            }
-        }
-
-        stage ('unit Tests') {
-            when {
-                branch 'demo'
-            }
-            steps {
-                echo 'Unit Test'
-            }
-        }
-
-        stage ('Download dependencies') {
-            when {
-                branch 'demo'
-            }
-            steps {
-                echo 'Download Dependencies'
-            }
-        }
-
-        stage ('Prepare Artifact') {
-            when { tag "*" }
-            steps {
-                echo 'Prepare Artifact'
-            }
-        }
-
-        stage ('Publish Artifact') {
-            when { tag "*" }
-            steps {
-                echo 'Publish Artifact'
-            }
-        }
-    }
+//        if (branch == "main" || tag ==~ "*" ) {
+//            stage('style checks') {
+//                echo 'code Quality'
+//            }
+//        }
+  }
 }
+
+
