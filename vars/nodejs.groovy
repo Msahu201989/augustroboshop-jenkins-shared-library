773a44d0d6960813e51692a4b39d8f61d60ee333
Mukesh <Stunty@007>
2023-08-07 20:53:43 +0530

    July 23 learning devops

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,33 +1,17 @@
-//def call() {
-//    node {
-//
-//        sh 'env'
-//        common.codequality()
-//
-//
-////        if (branch == "main" || tag ==~ "*" ) {
-////            stage('style checks') {
-////                echo 'code Quality'
-////            }
-////        }
-//  }
-//}
-//
-//
-
-
 def call() {
-
     node {
-        try {
-            common.codeCheckout()
-            common.codeQuality()
-            common.codeChecks()
-            common.artifacts()
-        } catch (Exception e) {
-            mail bcc: '', body: "Build Failed ${RUN_DISPLAY_URL}", cc: '', from: 'raghuk.vit@gmail.com', replyTo: '', subject: 'BUILD FAILURE', to: 'raghuk.vit@gmail.com'
-        }
 
-    }
+        sh 'env'
+        common.codequality()
 
+
+//        if (branch == "main" || tag ==~ "*" ) {
+//            stage('style checks') {
+//                echo 'code Quality'
+//            }
+//        }
+  }
 }
+
+
+
