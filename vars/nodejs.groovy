867b43cac46c8b50c7277271028cc8e43d0957e4
Mukesh <Stunty@007>
2023-08-09 20:07:06 +0530

    July 23 learning devops

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,14 +1,12 @@
 def call() {
     node {
-
-
-
+        try {
         common.codequality()
         common.codechecks()
         common.artifacts()
-
-
-
+    }  catch (Exception e) {
+        mail bcc: '', body: "Build Failure ${RUN_TESTS_DISPLAY_URL}", cc: '', from: 'mukeshsahu20@gmail.com', replyTo: '', subject: 'BUILD FAILURE', to: 'mukeshsahu20@gmail.com'
+    }
 
 
 
