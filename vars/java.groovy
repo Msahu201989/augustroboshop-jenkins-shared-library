749964ff14fbd2f2e488858b003f6d69f3b8ab9c
Mukesh <Stunty@007>
2023-08-13 12:11:45 +0530

    July 23 learning devops

--- vars/java.groovy
+++ vars/java.groovy
@@ -1,13 +1,19 @@
 def call() {
+    env.SONAR_EXTRA_OPTS = "-Dsonar.java.binaries=./target"
+    env.APPTYPE = "java"
     node {
         try {
-            common.codecheckout()
-            common.codequality()
-            common.codechecks()
+            common.codeCheckout()
+            stage('compile code'){
+                sh 'mvn compile'
+            }
+            common.codeQuality()
+            common.codeChecks()
             common.artifacts()
-        } catch (Exception e) {
+        }  catch (Exception e) {
             mail bcc: '', body: "Build Failure ${RUN_TESTS_DISPLAY_URL}", cc: '', from: 'mukeshsahu20@gmail.com', replyTo: '', subject: 'BUILD FAILURE', to: 'mukeshsahu20@gmail.com'
         }
 
     }
+
 }
