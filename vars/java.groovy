f6fb433f6151bc7155919960cd627da0f952963d
Mukesh <Stunty@007>
2023-08-14 21:48:03 +0530

    August 23 learning devops

--- vars/java.groovy
+++ vars/java.groovy
@@ -11,7 +11,7 @@ def call() {
             common.codeChecks()
             common.artifacts()
         }  catch (Exception e) {
-            mail bcc: '', body: "Build Failure ${RUN_TESTS_DISPLAY_URL}", cc: '', from: 'mukeshsahu20@gmail.com', replyTo: '', subject: 'BUILD FAILURE', to: 'mukeshsahu20@gmail.com'
+//            mail bcc: '', body: "Build Failure ${RUN_TESTS_DISPLAY_URL}", cc: '', from: 'mukeshsahu20@gmail.com', replyTo: '', subject: 'BUILD FAILURE', to: 'mukeshsahu20@gmail.com'
         }
 
     }
