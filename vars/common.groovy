3f450b20577d169eda6247cf2a5e07c2df58df00
Mukesh <Stunty@007>
2023-08-14 21:32:08 +0530

    August 23 learning devops

--- vars/common.groovy
+++ vars/common.groovy
@@ -10,7 +10,7 @@ def codequality() {
 //        withCredentials([usernamePassword(credentialsId: 'SONAR', passwordVariable: 'sonarPass', usernameVariable: 'sonarUser')]) {
 //            sh '''
 //        #sonar-scanner -Dsonar.host.url=http://172.31.10.133:9000 -Dsonar.login=${sonarUser} -Dsonar.password=${sonarPass} -Dsonar.projectKey=${COMPONENT} -Dsonar.qualitygate.wait=true ${SONAR_EXTRA_OPTS}
-//        echo OK
+        echo OK
 //      '''
         }
     }
